20a62831977906dabf73a81e90490f4aa93f30cc
JakeNHelfrich <jakenichelfrich@gmail.com>
2021-01-17 21:36:28 +0800

    touch up pt 3

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Text } from "react-native";
-
 import { LinearGradient } from "expo-linear-gradient";
-
-import Styles from "./styles";
+import styles from "./styles";
 import AppStyle from "../../styles";
 
 const Logo = () => {
@@ -12,10 +10,10 @@ const Logo = () => {
       colors={[AppStyle.orange, AppStyle.blue]}
       start={[0, 0.4]}
       locations={[0.5, 0.5]}
-      style={Styles.logo}
+      style={styles.logo}
     >
-      <Text style={[Styles.text, { marginRight: "10%" }]}>WORD</Text>
-      <Text style={[Styles.text, { marginLeft: "25%" }]}>LINK</Text>
+      <Text style={[styles.text, { marginRight: "10%" }]}>WORD</Text>
+      <Text style={[styles.text, { marginLeft: "25%" }]}>LINK</Text>
     </LinearGradient>
   );
 };
